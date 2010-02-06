8abfdf0ac4b27ecde97a922750ac9d5c8ae38403
plainwonders <plainwonders@86dfe8e4-096e-11df-b8b0-ddcda0e0916c>
2010-02-06 08:06:24 +0000

    Added a status bar class that can add icons from imagelist easily.

---
+++ BitmapImageList.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
+using DosboxApp.Properties;
+using Plain.Forms;
+
+namespace DosboxApp {
+	public class BitmapImageList : Plain.Forms.ResourceImageList {
+		public BitmapImageList() : base() { }
+
+		public BitmapImageList(IContainer container) : base(container) { }
+
+		public override void Refresh() {
+			base.Refresh();
+			foreach (DictionaryEntry entry in Resources.ResourceManager.GetResourceSet(CultureInfo.CurrentCulture, true, true)) {
+				Bitmap png = entry.Value as Bitmap;
+				if (png != null) {
+					base.ImageList.Images.Add(entry.Key as string, png);
+				}
+			}
+		}
+	}
+}
---
+++ Plain.Forms/ResourceImageList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using Plain.Design;
+
+namespace Plain.Forms {
+#if DEBUG
+	[Designer(typeof(ResourceImageListDesigner))]
+#endif
+	public abstract partial class ResourceImageList : Component {
+		public static implicit operator ImageList(ResourceImageList resiml) {
+			return resiml.ImageList;
+		}
+
+		public ResourceImageList() {
+			InitializeComponent();
+			m_iml = new ImageList();
+			create();
+		}
+
+		public ResourceImageList(IContainer container) {
+			container.Add(this);
+
+			InitializeComponent();
+			m_iml = new ImageList(container);
+			create();
+		}
+
+		public virtual void Refresh() {
+			m_iml.Images.Clear();
+		}
+
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+		public ImageList ImageList {
+			get { return m_iml; }
+		}
+
+		ImageList m_iml;
+
+		protected virtual ColorDepth ColorDepth {
+			get { return ColorDepth.Depth32Bit; }
+		}
+
+		void create() {
+			m_iml.ColorDepth = this.ColorDepth;
+			Refresh();
+		}
+	}
+}
