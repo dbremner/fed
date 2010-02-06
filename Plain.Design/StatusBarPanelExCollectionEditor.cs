478d69f9711a8a136eca8cc6fe664a1e01a50b88
plainwonders <plainwonders@86dfe8e4-096e-11df-b8b0-ddcda0e0916c>
2010-02-06 08:06:24 +0000

    Added a status bar class that can add icons from imagelist easily.

---
+++ Plain.Design/StatusBarPanelExCollectionEditor.cs
@@ -0,0 +1,32 @@
+﻿#if DEBUG
+
+using System;
+using System.ComponentModel.Design;
+using Plain.Forms;
+
+namespace Plain.Design {
+	public class StatusBarPanelExCollectionEditor : CollectionEditor {
+		public StatusBarPanelExCollectionEditor(Type type)
+			: base(type) {
+		}
+
+		protected override CollectionEditor.CollectionForm CreateCollectionForm() {
+			CollectionForm form = base.CreateCollectionForm();
+			form.Text = typeof(StatusBarPanelEx).Name + " Collection Editor";
+			return form;
+		}
+
+		protected override Type[] CreateNewItemTypes() {
+			Type[] types = base.CreateNewItemTypes();
+			Type[] types_ex = new Type[types.Length + 1];
+			int i = 0;
+			for (; i < types.Length; ++i) {
+				types_ex[i] = types[i];
+			}
+			types_ex[i] = typeof(StatusBarPanelEx);
+			return types_ex;
+		}
+	}
+}
+
+#endif
---
+++ Plain.Forms/StatusBarPanelEx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Design;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Plain.Native;
+
+namespace Plain.Forms {
+	public class StatusBarPanelEx : System.Windows.Forms.StatusBarPanel {
+		[Editor("System.Windows.Forms.Design.ImageIndexEditor, System.Design, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", typeof(UITypeEditor))]
+		[RefreshProperties(RefreshProperties.Repaint)]
+		[RelatedImageList("ParentEx.ResourceImageList.ImageList")]
+		[TypeConverterAttribute(typeof(ImageKeyConverter))]
+		public string ImageKey {
+			set {
+				m_ImageKey = value;
+				StatusBarEx sbrEx = this.ParentEx;
+				if (sbrEx != null) {
+					if (sbrEx.ResourceImageList != null) {
+						if (sbrEx.ResourceImageList.ImageList.Images.ContainsKey(m_ImageKey)) {
+							Image image = sbrEx.ResourceImageList.ImageList.Images[m_ImageKey];
+							if (image is Bitmap) {
+								base.Icon = Icon.FromHandle((image as Bitmap).GetHicon());
+							}
+						}
+					}
+				}
+			}
+			get { return m_ImageKey; }
+		}
+
+		[Browsable(false)]
+		public StatusBarEx ParentEx {
+			get { return base.Parent as StatusBarEx; }
+		}
+
+		string m_ImageKey;
+	}
+}
