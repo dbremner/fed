a53bb92fa46444590f3572b457b921b6b0e36a7b
plainwonders <plainwonders@86dfe8e4-096e-11df-b8b0-ddcda0e0916c>
2010-02-10 07:26:09 +0000

    Misc changes that should have been committed along.

---
+++ trunk/Plain.Forms/ClickThroughLabel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+using Plain.Native;
+
+namespace Plain.Forms {
+	public class ClickThroughLabel : System.Windows.Forms.Label {
+		protected override void WndProc(ref Message m) {
+			switch (m.Msg) {
+			case NativeMethods.WM_NCHITTEST:
+				m.Result = new IntPtr(NativeMethods.HTTRANSPARENT);
+				return;
+			}
+			base.WndProc(ref m);
+		}
+	}
+}
